aff4568dffd69295661292a1bcafa3be55fe314c
bacho <nicolassironi12@hotmail.com>
2023-06-20 12:04:22 -0300

    cambios realizados luego del mail de paola

--- api.js
+++ api.js
@@ -1,21 +1,46 @@
-const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'Accept-Encoding': 'application/gzip',
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-	},
-	body: new URLSearchParams({
-		q: 'English is hard, but detectably so'
-	})
-};
+// const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'application/x-www-form-urlencoded',
+// 		'Accept-Encoding': 'application/gzip',
+// 		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+// 		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+// 	},
+// 	body: new URLSearchParams({
+// 		q: 'English is hard, but detectably so'
+// 	})
+// };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
+  function redirectToTranslate() {
+    var apiKey = 'TU_CLAVE_DE_API'; // Reemplaza 'TU_CLAVE_DE_API' con tu propia clave de API
+    var targetUrl = 'https://translate.google.com/#auto/es/';
+    
+    var textToTranslate = prompt('Ingrese el texto a traducir:');
+    if (textToTranslate) {
+      var encodedText = encodeURIComponent(textToTranslate);
+      var translateUrl = 'https://translation.googleapis.com/language/translate/v2?key=' + apiKey + '&q=' + encodedText + '&target=es';
+
+      fetch(translateUrl)
+        .then(response => response.json())
+        .then(data => {
+          var translatedText = data.data.translations[0].translatedText;
+          targetUrl += encodeURIComponent(translatedText);
+
+          window.open(targetUrl, '_blank');
+        })
+        .catch(error => {
+          console.error('Error al traducir el texto:', error);
+        });
+    }
+  }
+
--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-let name = document.getElementById("exampleFormControlInput1");
-let telefono = document.getElementById("exampleFormControlInput2");
-let comentario = document.getElementById("exampleFormControlTextarea1");
+function validarFormulario() {
+  var email = document.getElementById("exampleFormControlInput1").value;
+  var telefono = document.getElementById("exampleFormControlInput2").value;
+  var comentario = document.getElementById("exampleFormControlTextarea1").value;
 
+  if (email === "" || telefono === "" || comentario === "") {
+    alert("Todos los campos deben ser completados");
+    return false;
+  }
 
-function enviarFormulario() {
-  alert('Enviando formulario...');
-  return false
+  // Validar el formato del correo electrónico utilizando una expresión regular
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("El correo electrónico no es válido");
+    return false;
+  }
+
+  // Si todos los campos son válidos, el formulario se envía
+  alert("Enviando formulario...");
+  return true;
 }
-
-
-
